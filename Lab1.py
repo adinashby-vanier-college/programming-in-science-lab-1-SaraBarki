9c95316033299ccaf36430f70fc8e3a733105251
SaraBarki <6290648@edu.vaniercollege.qc.ca>
2025-09-03 21:06:25 -0400

    SEVENTH ATTEMPT at fixing the layout (or eighth)

--- Lab1.py
+++ Lab1.py
@@ -1,16 +1,15 @@
 # Function 1: Write a simple Hello World program
 # This function should print "Hello, World!" to the screen.
 def hello_world():
-    print('Hello, World!')
+    print("Hello, World!")
 
 # Function 2: Get input and output with different variable types
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
-    name = input('Enter your name:')
-    age = int(input('Enter your age:'))
-    height = float(input('Enter your height:'))
-    print(f'Hello,{name}!')
-    print(f'You are {age} years old')
-    print(f'You are {height} meters')
-
+    name = input("Enter your name:")
+    age = int(input("Enter your age:"))
+    height = float(input("Enter your height:"))
+    print(f"Hello,{name}!")
+    print(f"You are {age} years old")
+    print(f"You are {height} meters")
