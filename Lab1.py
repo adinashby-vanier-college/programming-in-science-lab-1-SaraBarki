ec749e97c227b7a6cfa33c139dec3dcc5d49c129
SaraBarki <6290648@edu.vaniercollege.qc.ca>
2025-09-03 21:09:29 -0400

    TENTH attemp

--- Lab1.py
+++ Lab1.py
@@ -10,6 +10,6 @@ def input_output():
     name = input("Enter your name:")
     age = int(input("Enter your age:"))
     height = float(input("Enter your height:"))
-    print(f"Hello, {name} !")
+    print(f"Hello, {name}!")
     print(f"You are {age} years old.")
     print(f"You are {height} meters.")
